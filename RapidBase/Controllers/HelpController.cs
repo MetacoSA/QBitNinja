71afb90629c86d40dd09fb4881834801d773dd72
Nicolas DORIER <nicolas.dorier@gmail.com>
2014-12-31 00:26:28 +0100

    better help

--- RapidBase/Controllers/HelpController.cs
+++ RapidBase/Controllers/HelpController.cs
@@ -45,20 +45,21 @@ namespace RapidBase.Controllers
                     },
                     new RouteModel()
                     {
-                        Template = "whatisit/[address|txId|blockId|blockheader|base58|transaction|script|scriptbytes]",
+                        Template = "whatisit/[address|txId|blockId|blockheader|base58|transaction|signature|script|scriptbytes]",
                         Samples = new[]
                         {
-                            "whatisit/15sYbVpRh6dyWycZMwPdxJWD4xbfxReeHe",
-                            "whatisit/02012edcbdf6f8b7d4d315338423196ce1c4251ede5a8c9c1dfff645f67a008935",
-                            "whatisit/356facdac5f5bcae995d13e667bb5864fd1e7d59",
+                            "whatisit/15sYbVpRh6dyWycZMwPdxJWD4xbfxReeHe", //Address
+                            "whatisit/02012edcbdf6f8b7d4d315338423196ce1c4251ede5a8c9c1dfff645f67a008935", //public key
+                            "whatisit/356facdac5f5bcae995d13e667bb5864fd1e7d59", //Hash Pub key
+                            
+                            "whatisit/3P2sV4w1ZSk5gr6eePd6U2V56Mx5fT3RkD", //Address P2SH
+                            "whatisit/ea1bea7de1b975b962adbd57a9e0533449962a80", //Hash P2SH
+                            "whatisit/2103f670154f21dd26f558a5718776b3905d19ee83b01592255ea7b472d52d09d8baac", //Redeem Script
 
-                            "whatisit/3P2sV4w1ZSk5gr6eePd6U2V56Mx5fT3RkD",
-                            "whatisit/ea1bea7de1b975b962adbd57a9e0533449962a80",
+                            "whatisit/d3294938d12105a27a55af9c02864d6f633741bd5c5340a18972935a457275b9", //Transaction
 
-                            "whatisit/d3294938d12105a27a55af9c02864d6f633741bd5c5340a18972935a457275b9",
-
-                            "whatisit/0000000000000000080feeba134552a002aeb08e815aad1c41108f680d7b5f58",
-                            "whatisit/335598"
+                            "whatisit/0000000000000000080feeba134552a002aeb08e815aad1c41108f680d7b5f58", //Block
+                            "whatisit/335598", //Height
                         }
                     }
                 }
