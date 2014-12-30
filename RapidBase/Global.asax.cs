d5ef507b0f7e735b52b832bc07ac5402ae80007b
Nicolas DORIER <nicolas.dorier@gmail.com>
2014-12-30 12:06:46 +0100

    Update NBitcoin and indexer

--- RapidBase/Global.asax.cs
+++ RapidBase/Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using NBitcoin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
@@ -9,10 +11,32 @@ namespace RapidBase
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            Resolver = (RapidBaseDependencyResolver)GlobalConfiguration.Configuration.DependencyResolver;
+            Timer = new Timer(_ =>
+            {
+                Resolver.UpdateChain();
+            });
+            Timer.Change(0, (int)TimeSpan.FromSeconds(30).TotalMilliseconds);
+        }
+        protected void Application_End()
+        {
+            Timer.Dispose();
+        }
+
+        internal Timer Timer
+        {
+            get;
+            set;
+        }
+        internal RapidBaseDependencyResolver Resolver
+        {
+            get;
+            set;
         }
     }
 }
