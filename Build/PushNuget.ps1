1802859566d0edcfe540e94462d05140302da7cc
Nicolas DORIER <nicolas.dorier@gmail.com>
2016-02-20 20:34:23 +0900

    fix bug pushing to nuget

--- Build/PushNuget.ps1
+++ Build/PushNuget.ps1
@@ -7,7 +7,7 @@ nuget pack "../QBitNinja.Client/QBitNinja.Client.nuspec"
 nuget pack "../QBitNinja/QBitNinja.nuspec"
 
 forfiles /m *.nupkg /c "cmd /c C:/ProgF/NuGet/nuget.exe push @FILE"
-(((dir *.nupkg).Name) -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
+(((dir *.nupkg).Name)[0] -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
 $ver = $Matches.Item(0)
 git tag -a "v$ver" -m "$ver"
 git push --tags
