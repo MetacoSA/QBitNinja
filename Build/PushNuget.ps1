4de2ce25c44dd8ff898bcc88f50b9f71e057c697
Nicolas DORIER <nicolas.dorier@gmail.com>
2016-02-20 20:28:34 +0900

    add qbitninja server to nuget

--- Build/PushNuget.ps1
+++ Build/PushNuget.ps1
@@ -4,6 +4,7 @@ msbuild.exe "Build.csproj" /p:Configuration=Release
 msbuild.exe "PushNuget.csproj" /p:Configuration=Release
 
 nuget pack "../QBitNinja.Client/QBitNinja.Client.nuspec"
+nuget pack "../QBitNinja/QBitNinja.nuspec"
 
 forfiles /m *.nupkg /c "cmd /c C:/ProgF/NuGet/nuget.exe push @FILE"
 (((dir *.nupkg).Name) -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
--- QBitNinja/Properties/AssemblyInfo.cs
+++ QBitNinja/Properties/AssemblyInfo.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+#if SERVER
 [assembly: AssemblyTitle("QBitNinja")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,6 +15,16 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("QBitNinja.Tests")]
+#else
+[assembly: AssemblyTitle("QBitNinja.Client")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("QBitNinja")]
+[assembly: AssemblyProduct("QBitNinja.Client")]
+[assembly: AssemblyCopyright("Copyright © QBitNinja 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+#endif
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,7 +32,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ed067f8e-05d6-4cc5-82bb-3781be52a6b4")]
+//[assembly: Guid("ed067f8e-05d6-4cc5-82bb-3781be52a6b4")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +43,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.3.10")]
+[assembly: AssemblyVersion("1.0.3.10")]
+[assembly: AssemblyFileVersion("1.0.3.10")]
