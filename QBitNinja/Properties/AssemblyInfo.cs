c6725a05656027cc8d33534a8103b4c4ed9b7329
Nicolas DORIER <nicolas.dorier@gmail.com>
2016-03-26 03:56:18 +0900

    Update NBitcoin

--- QBitNinja/Properties/AssemblyInfo.cs
+++ QBitNinja/Properties/AssemblyInfo.cs
@@ -43,6 +43,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyInformationalVersion("1.0.3.13")]
-[assembly: AssemblyVersion("1.0.3.13")]
-[assembly: AssemblyFileVersion("1.0.3.13")]
+[assembly: AssemblyInformationalVersion("1.0.3.14")]
+[assembly: AssemblyVersion("1.0.3.14")]
+[assembly: AssemblyFileVersion("1.0.3.14")]
