ec3d4727c61f48fc0bc0ffe219c0359bd401afe9
NicolasDorier <nicolas.dorier@gmail.com>
2016-07-26 18:02:39 +0900

    version bump

--- QBitNinja/Properties/AssemblyInfo.cs
+++ QBitNinja/Properties/AssemblyInfo.cs
@@ -43,6 +43,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyInformationalVersion("1.0.3.21")]
-[assembly: AssemblyVersion("1.0.3.21")]
-[assembly: AssemblyFileVersion("1.0.3.21")]
+[assembly: AssemblyInformationalVersion("1.0.3.22")]
+[assembly: AssemblyVersion("1.0.3.22")]
+[assembly: AssemblyFileVersion("1.0.3.22")]
