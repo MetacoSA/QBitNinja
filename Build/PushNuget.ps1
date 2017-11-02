dc422f26c0bd993d02ea2f11b1ed767b1a4a0c3d
nicolas.dorier <nicolas.dorier@gmail.com>
2017-11-01 17:51:29 -0700

    Bump version, fix build

--- Build/PushNuget.ps1
+++ Build/PushNuget.ps1
@@ -9,10 +9,8 @@ cd ..\Build
 
 msbuild.exe "PushNuget.csproj" /p:Configuration=Release
 
-.\GitLink.exe ".." -ignore "QBitNinja.Tests,Build,QBitNinja.Client.Tests,QBitNinja.Hosting.Azure.Web,Common"
-
-nuget pack "../QBitNinja.Client/QBitNinja.Client.nuspec"
-nuget pack "../QBitNinja/QBitNinja.nuspec"
+./nuget.exe pack "../QBitNinja.Client/QBitNinja.Client.nuspec"
+./nuget.exe pack "../QBitNinja/QBitNinja.nuspec"
 
 forfiles /m *.nupkg /c "cmd /c NuGet.exe push @FILE -source https://api.nuget.org/v3/index.json"
 (((dir *.nupkg).Name)[0] -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
