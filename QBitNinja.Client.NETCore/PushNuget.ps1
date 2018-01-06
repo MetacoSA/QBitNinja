369fb017a4a43e56f6f036d2ac0897bed9a0008f
nicolas.dorier <nicolas.dorier@gmail.com>
2018-01-07 01:01:53 +0900

    Remove old package build system

---
+++ QBitNinja.Client.NETCore/PushNuget.ps1
@@ -0,0 +1,7 @@
+rm "bin\release\" -Recurse -Force
+dotnet build /p:Configuration=Release
+dotnet pack --no-build --no-restore --configuration Release
+dotnet nuget push "bin\Release" --source "https://api.nuget.org/v3/index.json"
+$ver = ((ls .\bin\release -File)[0].Name -replace '([^\.\d]*\.)+(\d+(\.\d+){1,3}).*', '$2')
+git tag -a "Client/v$ver" -m "Client/$ver"
+git push --tags
