f7a6d419fffa312180c56edbd56ff3549815edff
jorn vanloofsvelt <jorn_vanloofsvelt@hotmail.com>
2021-01-31 09:42:43 +0100

    Add docstring to ListenerTrace.

--- QBitNinja/ListenerTrace.cs
+++ QBitNinja/ListenerTrace.cs
@@ -8,13 +8,18 @@ using System.Threading.Tasks;
 
 namespace QBitNinja
 {
+    /// <summary>
+    /// Wrapper around a System.Diagnostics.TraceSource instance.
+    /// </summary>
     public static class ListenerTrace
     {
         static TraceSource _Source = new TraceSource("QBitNinja.Listener");
+
         public static void Error(string info, Exception ex)
         {
             _Source.TraceEvent(TraceEventType.Error, 0, info + "\r\n" + Utils.ExceptionToString(ex));
         }
+
         public static void Info(string info)
         {
             _Source.TraceInformation(info);
