c0e9bcd8770f5a97ba372c18278529b28467214a
jorn vanloofsvelt <jorn_vanloofsvelt@hotmail.com>
2021-01-31 09:41:00 +0100

    Add docstring to DefaultDataDirectory.

--- QBitNinja/DefaultDataDirectory.cs
+++ QBitNinja/DefaultDataDirectory.cs
@@ -5,13 +5,19 @@ using System.Text;
 
 namespace QBitNinja
 {
-    public class DefaultDataDirectory
+    public static class DefaultDataDirectory
     {
+		/// <summary>
+		/// Get or create a directory rooted in a location that is specified as an environment variable 
+		/// named either 'HOME', or 'APPDATA' (fallback).
+		/// </summary>
 		public static string GetDirectory(string appDirectory, string subDirectory, bool createIfNotExists = true)
 		{
 			string directory = null;
+
+			// Find path to 'appDirectory'.
 			var home = Environment.GetEnvironmentVariable("HOME");
-			if(!string.IsNullOrEmpty(home))
+			if (!string.IsNullOrEmpty(home))
 			{
 				directory = home;
 				directory = Path.Combine(directory, "." + appDirectory.ToLowerInvariant());
@@ -19,25 +25,30 @@ namespace QBitNinja
 			else
 			{
 				var localAppData = Environment.GetEnvironmentVariable("APPDATA");
-				if(!string.IsNullOrEmpty(localAppData))
+				if (!string.IsNullOrEmpty(localAppData))
 				{
 					directory = localAppData;
 					directory = Path.Combine(directory, appDirectory);
 				}
 				else
 				{
-					throw new DirectoryNotFoundException("Could not find suitable datadir");
+					throw new DirectoryNotFoundException("Could not find suitable datadir.");
 				}
 			}
-			if(!Directory.Exists(directory))
+
+
+			if (!Directory.Exists(directory))
 			{
 				Directory.CreateDirectory(directory);
 			}
+
+			// Find path to 'subdirectory' and create it if needed.
 			directory = Path.Combine(directory, subDirectory);
-			if(createIfNotExists && !Directory.Exists(directory))
+			if (createIfNotExists && !Directory.Exists(directory))
 			{
 				Directory.CreateDirectory(directory);
 			}
+
 			return directory;
 		}
 	}
