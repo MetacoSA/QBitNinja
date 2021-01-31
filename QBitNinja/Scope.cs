11fa145cf9b20ac6ce30e4a73fe40414dc91865e
jorn vanloofsvelt <jorn_vanloofsvelt@hotmail.com>
2021-01-31 09:43:15 +0100

    Add docstring to Scope.cs

--- QBitNinja/Scope.cs
+++ QBitNinja/Scope.cs
@@ -3,12 +3,24 @@ using System.Linq;
 
 namespace QBitNinja
 {
+    /// <summary>
+    /// Represents a scope for some part of a hierarchy, where each scope is represented by a number of names that make up a path
+    /// within that hierarchy. A scope covers all children in that path.
+    /// </summary>
     public class Scope
     {
+        /// <summary>
+        /// Create a scope for an empty path.
+        /// </summary>
         public Scope()
         {
-            _parents = new string[0];
+            Parents = new string[0];
         }
+
+        /// <summary>
+        /// Create a scope for a given path.
+        /// </summary>
+        /// <param name="parents">A path represented by a number of names, the name at the first index being the root.</param>
         public Scope(string[] parents)
         {
             if (parents == null)
@@ -17,28 +29,37 @@ namespace QBitNinja
             {
                 throw new ArgumentException("'/' is not authorized for naming a scope");
             }
-            _parents = parents;
+            Parents = parents;
         }
 
-        private readonly string[] _parents;
+        /// <summary>
+        /// A path represented by a number of names, the name at the first index being the root.
+        /// </summary>
         public string[] Parents
         {
-            get
-            {
-                return _parents;
-            }
+            get;
+            private set;
         }
 
+        /// <summary>
+        /// Get a child scope, which will be represented by the path of the current scope, extended with 
+        /// the given subpath.
+        /// </summary>
+        /// <param name="names">An array of names that make up a path rooted in the current scope.</param>
+        /// <returns></returns>
         public Scope GetChild(params string[] names)
         {
-            var parents = _parents.ToList();
+            var parents = Parents.ToList();
             parents.AddRange(names);
             return new Scope(parents.ToArray());
         }
 
+        /// <summary>
+        /// Join the parts of the path with forward slashes to get a string representation of the scope.
+        /// </summary>
         public override string ToString()
         {
-            return String.Join("/", _parents);
+            return String.Join("/", Parents);
         }
     }
 }
